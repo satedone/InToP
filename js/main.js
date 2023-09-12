dba4b5be4275c630afe05dc934c52b33c82fb189
satedone <nz@intop.dp.ua>
2023-09-12 15:50:34 +0300

    contact form

--- js/main.js
+++ js/main.js
@@ -15,3 +15,37 @@ buttons.forEach((button) => {
 
 // Ініціалізуємо першу кнопку як активну
 buttons[0].classList.add("active");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Отримайте необхідні елементи
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// Відкриття модального вікна при натисканні на кнопку
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Закриття модального вікна при натисканні на "x"
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Закриття модального вікна при натисканні на поза вікном
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ sendmessage.php
@@ -0,0 +1,18 @@
+<?php
+	$content = '';
+	foreach ($_POST as $key => $value) {
+		if ($value) {
+			$content .= "<b>$key</b>: <i>$value</i>\n";
+		}
+	}
+	if (trim($content)) {
+		$content = "<b>Message from Site: </b>\n" . $content;
+		$apiToken = "6381689532:AAGU990_BJjArTxUji-LwFgUQw8yPYM0o8U";
+		$data = [
+			'chat_id' => '@satedbots',
+			'text' => $content,
+			'parse_mode' => 'HTML'
+		];
+		$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data));
+	}
+?>
